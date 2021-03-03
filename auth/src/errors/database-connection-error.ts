3a2377b6642b00f171a76d1329142edd98268321
Narendran <narendev0610@gmail.com>
2021-03-03 23:43:05 +0530

    minor refactor by introducing a function in the databaseconnerrs and requestvaldiationerrs

--- auth/src/errors/database-connection-error.ts
+++ auth/src/errors/database-connection-error.ts
@@ -1,10 +1,17 @@
-
 export class DatabaseConnectionError extends Error {
+  statusCode = 500;
   reason = "Error connection to database";
   constructor() {
     super();
 
     // Only because we are extending a built in class
     Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+  }
+  serializeErrors() {
+    return [
+      {
+        message: this.reason,
+      },
+    ];
   }
 }
