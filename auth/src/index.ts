1c488b41f0db3aaeea987dcc4d5e3cdb36c27a81
Narendran <narendev0610@gmail.com>
2021-03-04 20:47:12 +0530

    refactored index.ts to accomadate isolated testing, test setup files done, inmemeory mongodb is used for isolated testing

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,39 +1,10 @@
-import express from "express";
-import "express-async-errors";
-import { json } from "body-parser";
 import mongoose from "mongoose";
-import cookieSession from 'cookie-session'
-
-import { currentUserRouter } from "./routes/current-user";
-import { signInRouter } from "./routes/signin";
-import { signOutRouter } from "./routes/signout";
-import { signUpRouter } from "./routes/signup";
-import { errorHandler } from "./middlewares/error-handler";
-import { NotFoundError } from "./errors/not-found-error";
-
-const app = express();
-app.set('trust proxy',true );
-app.use(json());
-app.use(cookieSession({
-  signed:false,
-  // secure:true  //! when this is set to true, it'll only work on connection coming with https://
-}))
-
-app.use(currentUserRouter);
-app.use(signInRouter);
-app.use(signOutRouter);
-app.use(signUpRouter);
-
-app.all("*", async () => {
-  throw new NotFoundError();
-});
-
-app.use(errorHandler);
+import { app } from "./app";
 
 // mongoose connection
 const start = async () => {
-  if(!process.env.JWTSECRET){
-    throw new Error('JWTSECRET must be defined')
+  if (!process.env.JWTSECRET) {
+    throw new Error("JWTSECRET must be defined");
   }
   try {
     await mongoose.connect("mongodb://auth-mongo-srv:27017/auth", {
@@ -41,8 +12,7 @@ const start = async () => {
       useUnifiedTopology: true,
       useCreateIndex: true,
     });
-    console.log('Connected to MongoDb');
-    
+    console.log("Connected to MongoDb");
   } catch (err) {
     console.error(err);
   }
