57a7fc430ac00e8fa97de252faa52e46ecb96f3b
Narendran <narendev0610@gmail.com>
2021-03-05 19:47:06 +0530

    moved errors & middlewares to common folder and publishing to common package

---
+++ common/build/middlewares/error-handler.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.errorHandler = void 0;
+var custom_errror_1 = require("../errors/custom-errror");
+var errorHandler = function (err, req, res, next) {
+    if (err instanceof custom_errror_1.CustomError) {
+        return res.status(err.statusCode).send({ errors: err.serializeErrors() });
+    }
+    res.status(400).send({
+        errors: [{ message: "Something went wrong" }],
+    });
+};
+exports.errorHandler = errorHandler;
