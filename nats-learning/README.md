a30434497e77125f4803878c5d714c65e24f40f7
Narendran <narendev0610@gmail.com>
2021-03-08 11:10:15 +0530

    nats streaming server, queue groups working done

--- nats-learning/README.md
+++ nats-learning/README.md
@@ -7,3 +7,11 @@ docs for nats streaming server at [docs.nats.io](docs.nats.io)
 this following article is a good read to know about nats/nats-streaming server and it's comparision with rabbitmq : [article](https://adam-kotwasinski.medium.com/rabbitmq-amqp-mostly-and-nats-nats-streaming-comparison-1f97753f1312)
 
 when using `ts-node-dev` we need to supply `--rs` flag to have reloading by typing `rs` in the terminal for restart enable because allow to restart with `rs` line entered in stdio, **disabled by default**.
+
+`queue group` - a queue group ensures that only one instance of the running service will get a event from the event bus and not all the instance of the same service listens it. This ensures the prevention of message duplication my multiple instance of a service get the event and prevents data duplication in the DB.
+
+`setManualAckMode` - when set to `true` this is manual acknowledgement mode which will send the event processed message to the nats server only the condition is met which is manually described by us. then nats streaming server will send the same event to the same or different instance  of that (unacknowledged) service [this will happen till the aknowledgement message is received]
+
+> ### Note:
+> NATS Streaming Server only have 1000 channels by default. 
+> we can change this number in settings
--- nats-learning/src/listener.ts
+++ nats-learning/src/listener.ts
@@ -1,22 +1,51 @@
-import nats ,{Message} from "node-nats-streaming";
+import nats, { Message } from "node-nats-streaming";
+import { randomBytes } from "crypto";
 
 console.clear();
 
-const stan = nats.connect("ticketing",'123', {
+// ! here we're randomly generating client ids for connection to nats streaming serve
+// ! by using randombytes function
+
+const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
   url: "http://nats-srv:4222",
 });
 
-stan.on('connect',() => {
-    console.log('Listener connected to NATS');
+stan.on("connect", () => {
+  console.log("Listener connected to NATS");
 
-  const subscription = stan.subscribe('ticket:created');
-
-  subscription.on('message', (msg: Message) => {
-    const data = msg.getData();
-
-    if (typeof data === 'string') {
-      console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
-    }
+  // ! setting up graceful shutdown that is to be sent to nats server from the client
+  stan.on("close", () => {
+    console.log("NATS connection closed!");
+    process.exit();
   });
 
-})
+  // all the options to be set must be chained on to this message , as opposed to traditional object passing
+  // ! don't use  the setdelivverallavailable options without considerations, because it'll dump all the event it has from the start to the service whenever service restarts
+  // # when using setDurableName() option, setDeliverAllAvailable() must be set 
+  const options = stan
+    .subscriptionOptions()
+    .setManualAckMode(true)
+    .setDeliverAllAvailable()
+    .setDurableName('order-service')
+
+  const subscription = stan.subscribe(
+    "ticket:created",
+    "orders-service-queue-group",
+    options
+  );
+
+
+  subscription.on("message", (msg: Message) => {
+    const data = msg.getData();
+
+    if (typeof data === "string") {
+      console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
+    }
+
+    //  this will send the acknowldgement message to the nats server
+    msg.ack();
+  });
+});
+
+process.on("SIGTERM", () => stan.close());
+process.on("SIGTERM", () => stan.close());
