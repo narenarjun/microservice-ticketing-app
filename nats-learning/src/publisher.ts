70cc90b2b7a93cb91d76550681e98274f292c0ae
Narendran <narendev0610@gmail.com>
2021-03-08 16:22:12 +0530

    base publisher abstract class created , subjects enum created, refactored publisher by using ticketcreatedpublisher class

--- nats-learning/src/publisher.ts
+++ nats-learning/src/publisher.ts
@@ -1,4 +1,5 @@
-import nats from 'node-nats-streaming';
+import nats from "node-nats-streaming";
+import { TicketCreatedPublisher } from "./events/ticket-created-publisher";
 
 console.clear();
 
@@ -6,20 +7,32 @@ console.clear();
 // ! when giving the client url for nats , http or https must be provided even if we're using inside
 // ! k8s cluster service discovery via k8s service
 
-const stan = nats.connect('ticketing', 'abc', {
-  url: 'http://nats-srv:4222',
+const stan = nats.connect("ticketing", "abc", {
+  url: "http://nats-srv:4222",
 });
 
-stan.on('connect', () => {
-  console.log('Publisher connected to NATS');
+stan.on("connect", async () => {
+  console.log("Publisher connected to NATS");
 
-  const data = JSON.stringify({
-    id: '123',
-    title: 'concert',
-    price: 20,
-  });
+  const publisher = new TicketCreatedPublisher(stan);
+  
+  try {
+    await publisher.publish({
+      id: "213",
+      title: "Concert 1",
+      price: 30,
+    });
+  } catch (err) {
+    console.error(err);
+  }
 
-  stan.publish('ticket:created', data, () => {
-    console.log('Event published');
-  });
+  // const data = JSON.stringify({
+  //   id: '123',
+  //   title: 'concert',
+  //   price: 20,
+  // });
+
+  // stan.publish('ticket:created', data, () => {
+  //   console.log('Event published');
+  // });
 });
