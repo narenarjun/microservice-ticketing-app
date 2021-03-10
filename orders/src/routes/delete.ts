17ec6ac9b68c91451968b18e9a66804f3b745f73
Narendran <narendev0610@gmail.com>
2021-03-10 12:50:10 +0530

    show and delete routes for orders service is implemented, tests for those routes are done,

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,9 +1,38 @@
+import {
+  NotAuthorizedError,
+  NotFoundError,
+  OrderStatus,
+  requireAuth,
+} from "@wowowow/common";
 import express, { Request, Response } from "express";
+import { Order } from "../models/order";
 
 const router = express.Router();
 
-router.delete("/api/orders/:orderId", async (req: Request, res: Response) => {
-  res.send({});
-});
+//  ! it maybe considered as PATCH method too
+router.delete(
+  "/api/orders/:orderId",
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const { orderId } = req.params;
+
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+
+    order.status = OrderStatus.Cancelled;
+    await order.save();
+
+    // an event must be published about this order being cancelled!
+
+    res.status(204).send(order);
+  }
+);
 
 export { router as deleteOrderRouter };
