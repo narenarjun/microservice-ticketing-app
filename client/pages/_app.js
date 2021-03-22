d10e36b39d343a29441ec2fc77b4fbcd34346592
Narendran <narendev0610@gmail.com>
2021-03-22 13:10:13 +0530

    all the pages corresponding to the service essentials are created, styling needs to be done and env variables should be introduced to store sensitive info.

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -7,21 +7,25 @@ const AppComponent = ({ Component, pageProps, currentUser }) => {
   return (
     <div>
       <Header currentUser={currentUser} />
-      <Component {...pageProps} />
+      <div className="container">
+      <Component currentUser={currentUser} {...pageProps} />
+      </div>
     </div>
   );
 };
 
 AppComponent.getInitialProps = async (appContext) => {
   const client = BuildClient(appContext.ctx);
-  const { data } = await client.get(
-    "http://localhost:4000/api/users/currentuser"
-  );
+  // const { data } = await client.get(
+  //   "http://localhost:4000/api/users/currentuser"
+  // );
+
+  const data = {}
 
   console.log("this is data from _app.js page", data);
   let pageProps = {};
   if (appContext.Component.getInitialProps) {
-    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx,client,data.currentUser);
   }
 
   return {
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,23 +1,39 @@
-import BuildClient from "../api/build-client";
+import Link from "next/link";
 
-const LandingPage = ({ currentUser }) => {
-  console.log(currentUser);
-  return currentUser ? (
-    <h1>You are signed in</h1>
-  ) : (
-    <h1>You are NOT signed in</h1>
+const LandingPage = ({ currentUser, tickets }) => {
+  const ticketList = tickets.map((ticket) => {
+    return (
+      <tr>
+        <td>{ticket.title}</td>
+        <td>{ticket.price}</td>
+        <td>
+          <Link href="/tickets/[ticketId]" as={`/tickets/${ticket.id}`}>
+            <a>View</a>
+          </Link>
+        </td>
+      </tr>
+    );
+  });
+  return (
+    <div>
+      <h1>Tickets</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Price</th>
+            <th>Link</th>
+          </tr>
+        </thead>
+        <tbody>{ticketList}</tbody>
+      </table>
+    </div>
   );
 };
 
-LandingPage.getInitialProps = async (context) => {
-  console.log("LANDING PAGE!");
-  const client = BuildClient(context);
-  const { data } = await client.get(
-    "http://localhost:4000/api/users/currentuser"
-  );
-  console.log("this is data from index.js page", data);
-
-  return data;
+LandingPage.getInitialProps = async (context, client, currentUser) => {
+  const { data } = await client.get("/api/tickets");
+  return { tickets: data };
 };
 
 export default LandingPage;
